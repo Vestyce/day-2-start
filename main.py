eadd7a4967a622e4612f93a7e3b14917306df523
Tony Zhu <118200735+Vestyce@users.noreply.github.com>
2023-02-17 20:04:05 +0000

    update time left calculator

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ height = 1.8
 isWinning = True
 #f-string 
 print(f"your score is{score}, your height is {height}, you are winning is {isWinning}")
+
+# time left calculator
+age = input("What is your current age? ")
+yearsleft = int(90) - int(age)
+monthsleft = yearsleft * 12
+weeksleft = yearsleft * 52)
+daysleft = yearsleft * 365
+print(f"You have {daysleft} days, {weeksleft} weeks, and {monthsleft} months left.")
+
